145193f49bdaef5b12489bd401829761aac2629e
raspi <gitraspigner@gmail.com>
2025-12-13 17:10:24 -0800

    add method signatures

--- capstone-api/src/main/java/org/yearup/data/ShoppingCartDao.java
+++ capstone-api/src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -1,9 +1,14 @@
 package org.yearup.data;
 
+import org.yearup.models.Product;
 import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
 
-public interface ShoppingCartDao
-{
+import java.util.List;
+
+public interface ShoppingCartDao {
     ShoppingCart getByUserId(int userId);
-    // add additional method signatures here
+    void addItem(int userId, ShoppingCartItem item);
+    void updateItem(int userId, int productId, int quantity);
+    void clearCart(int userId);
 }

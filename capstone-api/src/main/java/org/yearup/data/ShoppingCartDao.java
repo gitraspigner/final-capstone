8f81b2773fc7e077d0df8c59f994245a9c0c3213
raspi <gitraspigner@gmail.com>
2025-12-14 13:12:11 -0800

    add method signatures (for shoppingcartdao interface)

--- capstone-api/src/main/java/org/yearup/data/ShoppingCartDao.java
+++ capstone-api/src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -1,12 +1,16 @@
 package org.yearup.data;
 
-import org.yearup.models.Product;
 import org.yearup.models.ShoppingCart;
 import org.yearup.models.ShoppingCartItem;
 
-import java.util.List;
-
-public interface ShoppingCartDao {
+/**
+ * Data access interface for shopping cart operations.
+ * Defines the contract implemented by MySqlShoppingCartDao.
+ *
+ * @author Ravi Spigner
+ */
+public interface ShoppingCartDao
+{
     ShoppingCart getByUserId(int userId);
     void addItem(int userId, ShoppingCartItem item);
     void updateItem(int userId, int productId, int quantity);

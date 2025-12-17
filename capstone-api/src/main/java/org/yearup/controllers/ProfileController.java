94b3d1d2d590afb565b38bdac5f53c0f81c3d622
raspi <gitraspigner@gmail.com>
2025-12-17 11:06:33 -0800

    remove unnecessary comments

--- capstone-api/src/main/java/org/yearup/controllers/ProfileController.java
+++ capstone-api/src/main/java/org/yearup/controllers/ProfileController.java
@@ -13,19 +13,16 @@ public class ProfileController {
         this.profileDao = profileDao;
     }
 
-    // Registration (create new profile)
     @PostMapping
     public Profile register(@RequestBody Profile newProfile) {
         return profileDao.create(newProfile);
     }
 
-    // Fetch profile by userId
     @GetMapping("/{userId}")
     public Profile getProfile(@PathVariable int userId) {
         return profileDao.getByUserId(userId);
     }
 
-    // Update existing profile
     @PutMapping("/{userId}")
     public Profile updateProfile(@PathVariable int userId, @RequestBody Profile updatedProfile) {
         updatedProfile.setUserId(userId);
